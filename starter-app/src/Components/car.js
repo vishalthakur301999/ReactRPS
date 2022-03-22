a8bd9591ed5afb64c15ba3a16a716d82ef6ee1a8
Vishal Thakur <vishalthakur301999@gmail.com>
2022-03-22 12:59:40 +0530

    State

--- starter-app/src/Components/car.js
+++ starter-app/src/Components/car.js
@@ -1,22 +1,51 @@
 import { Component } from 'react';
-import { Card } from '@geist-ui/core'
+import { Card, Button, Spacer } from '@geist-ui/core'
 
 class Car extends Component {
     constructor(props) {
         super(props)
 
         this.state = {
-            brand: "Maruti",
+            Brand: "Maruti Suzuki",
             Color: "Silver",
             Model: "Swift Dzire"
         }
+    }
 
+    handleModel = (e) => {
+        if(this.state.Model === "Swift Dzire"){
+            this.setState({
+                Model: "Ciaz"
+            })
+        }else {
+            this.setState({
+                Model: "Swift Dzire"           
+            })
+        }
+    }
+
+    handleColor = (e) => {
+        if(this.state.Color === "Silver"){
+            this.setState({
+                Color: "Black"
+            })
+        }else {
+            this.setState({
+                Color: "Silver"
+            })
+        }
     }
 
     render() {
+        const {Model, Color, Brand} = this.state;
         return (
             <Card hoverable>
-                <p>I have a {this.state.brand} {this.state.Model} which is {this.state.Color} in color</p>
+                <p>I have a {Brand} {Model} which is {Color} in color</p>
+                <Spacer h={.5} />
+                <Button onClick={this.handleModel}>Change Model</Button>
+                <Spacer h={.5} />
+                <Button onClick={this.handleColor}>Change Color</Button>
+                <Spacer h={.5} />
             </Card>
         )
     }
